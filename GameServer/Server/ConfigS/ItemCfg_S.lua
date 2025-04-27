701f744aab33ff9a94115fc08054720849efba82
GaoZhu <gaozhu@gyyx.cn>
2025-04-27 09:33:49 +0800

    implement grpc gateway for http-to-internal-service routing

---
+++ GameServer/Server/ConfigS/ItemCfg_S.lua
@@ -0,0 +1,5 @@
+return
+{
+[1]={Id=1,Type=1},
+[2]={Id=2,Type=2},
+[3]={Id=3,Type=3}}
---
+++ GameServer/Server/ConfigS/ParamCfg_S.lua
@@ -0,0 +1,8 @@
+return
+{
+["EverydayMapGetGoldCount"]={Id="EverydayMapGetGoldCount",IntParam=10,IntParams={},TextParam=""},
+["EarningsSpeedMultiplier"]={Id="EarningsSpeedMultiplier",IntParam=2,IntParams={},TextParam=""},
+["GetFreeSpeedUpIntervalTime"]={Id="GetFreeSpeedUpIntervalTime",IntParam=120,IntParams={},TextParam=""},
+["EarningsSpeedUpTime"]={Id="EarningsSpeedUpTime",IntParam=0,IntParams=[10,75,240],TextParam=""},
+["EarningsSpeedUpDiamond"]={Id="EarningsSpeedUpDiamond",IntParam=0,IntParams=[0,10,20],TextParam=""},
+["CarCultivateMaxLevel"]={Id="CarCultivateMaxLevel",IntParam=1000,IntParams={},TextParam=""}}
