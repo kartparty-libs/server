9762af7311778bde536aea56e2c8a568d4a09e17
FaMack <463378412@qq.com>
2025-04-27 16:25:39 +0800

    add configuration center with versioned key-value store

--- GameServer/Server/ConfigS/CarUpgradeCfg_S.lua
+++
@@ -1,7 +0,0 @@
-return
-{
-[1]={Id=1,ModuleType=0,Level=1,UpgradeCost=1000,ProductSeconds=5},
-[2]={Id=2,ModuleType=1,Level=1,UpgradeCost=66,ProductSeconds=0},
-[3]={Id=3,ModuleType=2,Level=1,UpgradeCost=67,ProductSeconds=0},
-[4]={Id=4,ModuleType=3,Level=1,UpgradeCost=55,ProductSeconds=0},
-[5]={Id=5,ModuleType=4,Level=1,UpgradeCost=98,ProductSeconds=0}}
--- GameServer/Server/ConfigS/ShopCfg_S.lua
+++
@@ -1,6 +0,0 @@
-return
-{
-[1]={Id=1,CostType=2,Cost="20",GiftId=1},
-[2]={Id=2,CostType=2,Cost="20",GiftId=1},
-[3]={Id=3,CostType=2,Cost="25",GiftId=1},
-[4]={Id=4,CostType=2,Cost="30",GiftId=1}}
